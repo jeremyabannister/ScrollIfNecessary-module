1ffba70a0d380344984a982ce0e3810c5677d666
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-04-28 02:11:02 +0200

    Initial Commit

---
+++ Sources/ScrollIfNecessary-module/ScrollIfNecessary_module.swift
@@ -0,0 +1,6 @@
+public struct ScrollIfNecessary_module {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ScrollIfNecessary-moduleTests/ScrollIfNecessary_moduleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ScrollIfNecessary_module
+
+final class ScrollIfNecessary_moduleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ScrollIfNecessary_module().text, "Hello, World!")
+    }
+}
